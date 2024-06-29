23081cadb6e0feecb90fd7928b5172ea3f93fcc6
GiorgiGogsadze <gogsadzegeorge42@gmail.com>
2024-06-29 15:23:56 +0400

    trying deployment with env variables

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,6 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+console.log(supabaseUrl);
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
