c958263fc098bb2d016960cbb6a48cfb492a3f60
GiorgiGogsadze <gogsadzegeorge42@gmail.com>
2024-06-29 15:26:52 +0400

    probably done

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,8 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log(supabaseUrl);
-
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
