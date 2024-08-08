8c738783a917b583077c6aa3e7ed8b66f0ae6209
GiorgiGogsadze <gogsadzegeorge42@gmail.com>
2024-08-08 12:40:59 +0400

    add uploader

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ import Checkin from "./pages/Checkin";
 import ProtectedRoute from "./ui/ProtectedRoute";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import InternetGate from "./ui/InternetGate";
-
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
